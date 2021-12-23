3fc761c38a110aedfd565fd4b98bd472b0fc10ca
qufaud2009 <96227819+qufaud2009@users.noreply.github.com>
2021-12-23 17:53:32 +0900

    Create main.py

---
+++ 2강/main.py
@@ -0,0 +1,41 @@
+year = 2021
+print (year)
+red_penprice = 1000
+print (red_penprice)
+print ("\"파이썬은 쉽다\"라고 '철수'가 말했어요.")
+print ('"파이썬은 쉽다"라고 \'철수\'가 말했어요.')
+print ("\"파이썬은 쉽다\"라고 \'철수\'가 말했어요.")
+print (""""파이썬은 쉽다"라고 '철수'가 말했어요.""")
+print ('''"파이썬은 쉽다"라고 '철수'가 말했어요.''')
+head = "오늘 날씨는 "
+tail = "맑습니다."
+print(head+tail)
+string = '''오늘 날씨는 맑습니다.
+나는 오늘 라이언 일병 구하기 피규어를 받습니다.'''
+print (string)
+print ("=" * 50)
+print ("my program")
+print ("=" * 50)
+hello = "\"안녕.\" 철수가 인사를 했다."
+print (hello[10])
+a = "Life is too short, You need python."
+print(a[-6])
+print (a[0])
+print (a[8])
+print (a[2])
+print (a[9])
+print(a [0:5])
+print(a [:17])
+print(a [19:])
+print(a [:])
+print("Hi"*9)
+s1 = "I am"
+s2 = "Ironman"
+s3 = s1 + '  ' + s2 + '  '
+print (s3 * 9)
+name1="리처드 윈터스"
+age1=27
+name2="privte jackson"
+age2=27
+print("이름: %s 나이: %d" %(name1, age1))
+print("이름: %s 나이: %d" %(name2, age2))
